22e40ff96c6ac29dd7354434ff72040d43eb1594
kolgoferRoman <roman.kolgofer@gmail.com>
2023-03-25 14:40:30 +0200

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,21 +1,12 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  slidesPerView: 4,
+  spaceBetween: 18,
 });
